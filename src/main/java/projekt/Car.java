b8edcf803d4a5c04ba2288e1ba7103bc2832ecbe
Alejandro Restrepo Klinge <56825548+mrrestre@users.noreply.github.com>
2019-12-02 20:40:24 +0100

    Parking Area and Car where implemented (not complete)

--- src/main/java/projekt/Car.java
+++ src/main/java/projekt/Car.java
@@ -1,13 +1,63 @@
 package projekt;
 
 public class Car {
+
+    /* /////////////////////Attributes///////////////////////// */
+
     private int carID;
-    private String type;
+    private Type type;
+    public enum Type{
+        MINI,
+        LITTLE,
+        UTILITARIAN,
+        COMPACT,
+        MINIVAN,
+        MIDDLE,
+        VAN,
+        SUV;
+    }
+    private String brand;
     private String model;
-    private String state;
-    private int parkID;
-    private String transmission;
-    private String fuelType;
-    private String GPS;
+    private State state;
+    public enum State{
+        PERFECT,
+        OK,
+        DAMAGED;
+    }
+    private Transmission transmission;
+    public enum Transmission{
+        MANUAL,
+        AUTOMATIC;
+    }
+    private FuelType fuelType;
+    public enum FuelType{
+        PETROL,
+        DIESEL,
+        HYBRID,
+        ELECTRIC;
+    }
+    private double GPSLongitude = 0.00;
+    private double GPSLatitude = 0.00;
+
+    /* /////////////////////Methods/////////////////////////// */
+
+    public void Car(Type type, String brand, String model, State state, Transmission transmission, FuelType fuelType){
+        // A Car manager should be done and here somehow the ID should be given
+        this.type = type;
+        this.brand = brand;
+        this.model = model;
+        this.state = state;
+        this.transmission = transmission;
+        this.fuelType = fuelType;
+    }
+
+    public void changeCarState(State newCarState){
+        this.state = newCarState;
+    }
+
+    public void setNewGPSlocation(double longitude, double latitude){
+        this.GPSLongitude = longitude;
+        this.GPSLatitude = latitude;
+    }
 
 }
--- src/main/java/projekt/ParkingArea.java
+++ src/main/java/projekt/ParkingArea.java
@@ -1,10 +1,67 @@
 package projekt;
+import java.util.*;
 
 public class ParkingArea {
+
+    /* /////////////////////Attributes///////////////////////// */
+
     private int parkID;
     private int parkZIP;
     private String parkCity;
     private String parkStreet;
     private String parkHouseNumber;
-    private int capacity;
+    // ArrayList in case the number of cars in station changes
+    private ArrayList<Integer> stationCarIDList = new ArrayList<Integer>();
+    private ArrayList<Integer> availableCarIDList = new ArrayList<Integer>();
+    private ArrayList<Integer> notAvailableCarIDList = new ArrayList<Integer>();
+
+    /* /////////////////////Methods/////////////////////////// */
+
+    public ParkingArea(int parkZIP, String parkCity, String parkStreet, String parkHouseNumber) {
+        // A ParkingArea manager should be done and here somehow the ID should be given
+        this.parkZIP = parkZIP;
+        this.parkCity = parkCity;
+        this.parkStreet = parkStreet;
+        this.parkHouseNumber = parkHouseNumber;
+    }
+
+    public void assignCarToStation (int carID){
+        stationCarIDList.add(carID);
+        availableCarIDList.add(carID);
+    }
+
+    private int getIndexInStationCarIDList (int carID){
+        int carIDIndex;
+        for (carIDIndex = 0; carIDIndex < stationCarIDList.size(); carIDIndex++){
+            if (carID == stationCarIDList.get(carIDIndex)){
+                break;
+            }
+        }
+        return carIDIndex;
+    }
+
+    public void removeCarFromStation (int carID){
+        int currentCarIndex = getIndexInStationCarIDList(carID);
+        stationCarIDList.remove(currentCarIndex);
+        availableCarIDList.remove(currentCarIndex);
+    }
+
+
+    public void carIsBeingUsed (int carID){
+        int currentCarIndex = getIndexInStationCarIDList(carID);
+        availableCarIDList.remove(currentCarIndex);
+        notAvailableCarIDList.add(carID);
+    }
+
+    public void carIsNoLongerBeingUsed (int carID){
+        int currentCarIndex = getIndexInStationCarIDList(carID);
+        availableCarIDList.add(carID);
+        notAvailableCarIDList.remove(currentCarIndex);
+    }
+
+    public int numberOfCarsAssignedToStation (){
+        return stationCarIDList.size();
+    }
+
+
 }
