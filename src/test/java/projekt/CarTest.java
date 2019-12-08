faa3377b47c2c388b34c0059708f49b2be2715c8
TheBenitoo <benitograuel@googlemail.com>
2019-12-08 23:41:41 +0100

    Added CarTest, ParkingAreaTest, CustomerTest. Not finished!!

--- src/test/java/projekt/CarTest.java
+++ src/test/java/projekt/CarTest.java
@@ -1,7 +1,39 @@
 package projekt;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
 
+    List cars = new List();
+    Car car1 = new Car(Car.Type.MIDDLE, "Mercedes", "S500", Car.State.PERFECT,
+            Car.Transmission.AUTOMATIC, Car.FuelType.HYBRID, 13, 300, 100, cars);
+    //List rentals = new List();
+    //Rental rental1 = new Rental(car1, 1, format.parse() , 12, 13, rentals);
+
+
+    @Test
+    public void test_Get_Fuel_Type() {
+
+        assertTrue(car1.getFuelType() == Car.FuelType.HYBRID);
+    }
+
+    @Test
+    public void should_change_Car_State() {
+
+        car1.changeCarState(Car.State.OK);
+
+        assertTrue(Car.State.OK == car1.getCarState());
+    }
+
+    @Test
+    public void test_CarID() {
+        assertTrue(car1.getCarID() == 0);
+    }
 }
+
+
--- src/test/java/projekt/CustomerTest.java
+++ src/test/java/projekt/CustomerTest.java
@@ -1,7 +1,31 @@
 package projekt;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.GregorianCalendar;
 
 class CustomerTest {
 
+    List customers = new List();
+    Customer customer1 = new Customer("Mustermann", "Max", "99099","Marktstraße",
+            "12",new GregorianCalendar(1990,GregorianCalendar.AUGUST,14),
+             Customer.CustomerType.REGULARUSER,"1", Customer.CardType.CREDITCARD,
+            new GregorianCalendar(2019,GregorianCalendar.DECEMBER,31),"STU56BN",customers);
+
+
+   /* @Test
+    public void test_If_State_Is_Changed_When_Customer_Damages_Car () {
+
+        customer1.customerDamagesCar();
+    } */
+
+   @Test
+    public void should_Change_Payment_Method () {
+
+       customer1.modifyPaymentMethod("2", Customer.CardType.CREDITCARD,
+               new GregorianCalendar(2020,GregorianCalendar.DECEMBER,31), "VKN85JK");
+
+       assertEquals(customer1.getCardNumber(),"2");
+   }
 }
