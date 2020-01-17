afb95e7f3dce6675c7bc2a179e77dcc5a6125539
Alejandro Restrepo Klinge <56825548+mrrestre@users.noreply.github.com>
2020-01-17 11:37:23 +0100

    maven

--- src/main/java/Car/CarManagementSystem.java
+++ src/main/java/Car/CarManagementSystem.java
@@ -3,16 +3,10 @@ package Car;
 import java.util.ArrayList;
 
 public class CarManagementSystem {
-    public ArrayList<TraditionalCar> traditionalCars;
-    public ArrayList<UtilitarianCar> utilitarianCars;
-    public ArrayList<HybridCar> hybridCars;
+    public ArrayList<CombustionCar> traditionalCars;
     public ArrayList<ElectricCar> electricCars;
 
-    private int traditionalCarsCounter;
-    private int utilitarianCarsCounter;
-    private int hybridCarsCounter;
+    private int combustionCarsCounter;
     private int electricCarsCounter;
-
-
 
 }
