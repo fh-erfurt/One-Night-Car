5152c3ec778e6301f9ab8e13d3db9d39b46fae72
pascalgiese <pascal.giese.uni@gmail.com>
2020-01-31 21:02:18 +0100

    - added RentalManager - changed Datelibrary to LocalDate and LocalDateTime - tried first test

--- src/test/java/Rental/RentalTest.java
+++ src/test/java/Rental/RentalTest.java
@@ -1,5 +1,28 @@
 package Rental;
 
+import Car.CarManagementSystem;
+import Car.ElectricCar;
+import Person.Customer;
+import Person.PersonManager;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 class RentalTest {
+
+    @Test
+    void testElapsedDays(){
+        RentalManager Rentals = new RentalManager();
+        CarManagementSystem CarManagementSystem = new CarManagementSystem();
+        ElectricCar ElectricCar = new ElectricCar(CarManagementSystem);
+        PersonManager PersonManager = new PersonManager();
+        Customer customer = new Customer(PersonManager);
+        ElectricRental TeslaS = new ElectricRental(Rentals, CarManagementSystem, customer, ElectricCar, PersonManager);
+
+        int elapsedDays = TeslaS.calculateElapsedDays();
+        assertEquals(7, elapsedDays);
+    }
+
+    @Test
+
 
 }
