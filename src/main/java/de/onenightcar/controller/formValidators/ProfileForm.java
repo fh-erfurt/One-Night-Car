86a7e78cf86796e90ee845889664502461cf0ca7
TheBenitoo <benitograuel@googlemail.com>
2020-08-06 15:05:27 +0200

    Profile Beta(Css in Arbeit)

---
+++ src/main/java/de/onenightcar/controller/formValidators/ProfileForm.java
@@ -0,0 +1,10 @@
+package de.onenightcar.controller.formValidators;
+
+public class ProfileForm {
+
+    private String firstName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+}
